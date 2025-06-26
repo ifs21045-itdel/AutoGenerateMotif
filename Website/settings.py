57ea1ee8595aa13a668168a5972ee51a397dfb6c
ifs21045-itdel <ifs21045@student.del.ac.id>
2025-06-26 22:11:32 +0700

    Memperbaharui ukuran

--- Website/settings.py
+++ Website/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD':'root',  # Sesuaikan dengan password MariaDB Anda
         'HOST': 'localhost',  # atau 'localhost'
-        'PORT':'3306',
+        'PORT':'3307',
 }
 }
 
